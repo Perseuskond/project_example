a5b9641af92d1cf84363b7afe11f39d9b4ce5e0e
Rick <gitlearningtest123@gmail.com>
2019-04-21 00:26:14 +0600

    Delete third line

--- example/java/Helloworld.java
+++ example/java/Helloworld.java
@@ -4,6 +4,5 @@ class Helloworld{
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Type something");
-        System.out.println("Scanner");
     }
 }
