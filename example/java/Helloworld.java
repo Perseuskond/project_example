b4c2c9722f851c0d4a94f37220280bbf83e04e50
Perseuskond <49819882+Perseuskond@users.noreply.github.com>
2019-04-21 01:40:51 +0600

    Small minor addition

--- example/java/Helloworld.java
+++ example/java/Helloworld.java
@@ -3,5 +3,7 @@ package example.java;
 class Helloworld{
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("String");
+        
     }
 }
