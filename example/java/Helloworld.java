3413615af004f7072dd5f004a17d5466408b7fed
Rick <gitlearningtest123@gmail.com>
2019-04-21 01:26:34 +0600

    Another small change

--- example/java/Helloworld.java
+++ example/java/Helloworld.java
@@ -3,6 +3,5 @@ package example.java;
 class Helloworld{
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("Type something");
     }
 }
