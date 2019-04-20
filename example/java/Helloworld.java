e40d8ed50335572515eaf765a9122672df4ff8ae
Rick <gitlearningtest123@gmail.com>
2019-04-20 23:57:32 +0600

    Add new string

---
+++ example/java/Helloworld.java
@@ -0,0 +1,8 @@
+package example.java;
+
+class Helloworld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Type something");
+    }
+}
