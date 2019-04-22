7394c86a49dff7c045beded1443951109c74daf5
Rick <gitlearningtest123@gmail.com>
2019-04-22 15:08:04 +0600

    Comment added

--- example/java/Helloworld.java
+++ example/java/Helloworld.java
@@ -4,6 +4,7 @@ class Helloworld{
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("String");
+	   //add comment here
         
     }
 }
